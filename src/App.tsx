53077a16787b18a204a6d07ab4afd825d0d3809e
Dricc-new <diegoramoncc17@gmail.com>
2023-08-05 17:20:26 -0400

    modified app

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ function App() {
             <h1>Please touch the button to verify the connection to the server</h1>
             <button onClick={async () => {
                 try{
-                    await fetch('http://localhost:3000/ping')
+                    const res = await fetch('http://localhost:3000/ping')
+                    console.log(res.text())
                     setConnection(true)
                 }catch{
                     setConnection(false)
                 }
-                
             }}>Verify</button>
             {
                 connection?<h2 style={{color:'#60ff60'}}>Connected</h2>:<h2 style={{color:'#ff6060'}}>Disconnected</h2>
