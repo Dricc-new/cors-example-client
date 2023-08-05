d7660995f4c756007bd6052eb6b5f131ed217db5
Dricc-new <diegoramoncc17@gmail.com>
2023-08-05 16:49:16 -0400

    app modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [connection, setConnection] = useState(false)
+    return (
+        <>
+            <h1>Please touch the button to verify the connection to the server</h1>
+            <button onClick={async () => {
+                try{
+                    await fetch('http://localhost:3000/ping')
+                    setConnection(true)
+                }catch{
+                    setConnection(false)
+                }
+                
+            }}>Verify</button>
+            {
+                connection?<h2 style={{color:'#60ff60'}}>Connected</h2>:<h2 style={{color:'#ff6060'}}>Disconnected</h2>
+            }
+        </>
+    )
 }
 
 export default App
